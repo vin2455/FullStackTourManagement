a1be46f6035bd2447d6605e125658f18c252fea7
Vinay Kumar Gandra <v-vinga@microsoft.com>
2018-06-13 15:07:29 +0530

    syntax fixes

--- TourManagement.Client/src/app/app.module.ts
+++ TourManagement.Client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { DatePipe } from '@angular/common'
+import { DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http/src/client';
 
 // import routing module
@@ -10,12 +10,14 @@ import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about';
 import {
-  ToursComponent, TourAddComponent, TourDetailComponent,
+  ToursComponent,
+  TourAddComponent,
+  TourDetailComponent,
   TourUpdateComponent
 } from './tours';
 import { ShowsComponent, ShowAddComponent } from './tours/shows';
-import { TourService } from './tours/shared/tour.service'
-import { ShowService } from './tours/shows/shared/show.service'
+import { TourService } from './tours/shared/tour.service';
+import { ShowService } from './tours/shows/shared/show.service';
 import { MasterDataService } from './shared/master-data.service';
 import { GlobalErrorHandler } from './shared/global-error-handler';
 import { ErrorLoggerService } from './shared/error-logger.service';
@@ -69,46 +71,106 @@ import { RedirectSilentRenewComponent } from './redirect-silent-renew/redirect-s
     {
       provide: HTTP_INTERCEPTORS,
       useClass: HandleHttpErrorInterceptor,
-      multi: true,
+      multi: true
     },
-    GlobalErrorHandler, ErrorLoggerService, TourService, 
-    MasterDataService, ShowService, DatePipe, OpenIdConnectService,
-  RequireAuthenticatedUserRouteGuardService],
+    GlobalErrorHandler,
+    ErrorLoggerService,
+    TourService,
+    MasterDataService,
+    ShowService,
+    DatePipe,
+    OpenIdConnectService,
+    RequireAuthenticatedUserRouteGuardService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
-
   constructor() {
-
     // automapper mappings
 
-    automapper.createMap('TourFormModel', 'TourForCreation')
-      .forSourceMember('band', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forSourceMember('manager', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forMember('bandid', function (opts) { opts.mapFrom('band'); });
+    automapper
+      .createMap('TourFormModel', 'TourForCreation')
+      .forSourceMember(
+        'band',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forSourceMember(
+        'manager',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forMember('bandid', function(opts) {
+        opts.mapFrom('band');
+      });
 
-    automapper.createMap('TourFormModel', 'TourWithManagerForCreation')
-      .forSourceMember('band', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forSourceMember('manager', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forMember('bandid', function (opts) { opts.mapFrom('band'); })
-      .forMember('managerid', function (opts) { opts.mapFrom('manager'); })
+    automapper
+      .createMap('TourFormModel', 'TourWithManagerForCreation')
+      .forSourceMember(
+        'band',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forSourceMember(
+        'manager',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forMember('bandid', function(opts) {
+        opts.mapFrom('band');
+      })
+      .forMember('managerid', function(opts) {
+        opts.mapFrom('manager');
+      });
 
-    automapper.createMap('TourFormModel', 'TourWithShowsForCreation')
-      .forSourceMember('band', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forSourceMember('manager', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => { opts.ignore(); })
-      .forMember('bandid', function (opts) { opts.mapFrom('band'); });
+    automapper
+      .createMap('TourFormModel', 'TourWithShowsForCreation')
+      .forSourceMember(
+        'band',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forSourceMember(
+        'manager',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forMember('bandid', function(opts) {
+        opts.mapFrom('band');
+      });
 
-    automapper.createMap('TourFormModel', 'TourWithManagerAndShowsForCreation')
-      .forSourceMember('band', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => 
-      { opts.ignore(); })
-      .forSourceMember('manager', (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => 
-      { opts.ignore(); })
-      .forMember('bandid', function (opts) { opts.mapFrom('band'); })
-      .forMember('managerid', function (opts) { opts.mapFrom('manager'); })
+    automapper
+      .createMap('TourFormModel', 'TourWithManagerAndShowsForCreation')
+      .forSourceMember(
+        'band',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forSourceMember(
+        'manager',
+        (opts: AutoMapperJs.ISourceMemberConfigurationOptions) => {
+          opts.ignore();
+        }
+      )
+      .forMember('bandid', function(opts) {
+        opts.mapFrom('band');
+      })
+      .forMember('managerid', function(opts) {
+        opts.mapFrom('manager');
+      });
 
-      automapper.createMap('ShowCollectionFormModelShowsArray', 
-      'ShowCollectionForCreation');
+    automapper.createMap(
+      'ShowCollectionFormModelShowsArray',
+      'ShowCollectionForCreation'
+    );
 
-      automapper.createMap('TourFormModel', 'TourForUpdate');
+    automapper.createMap('TourFormModel', 'TourForUpdate');
   }
 }
